8ad9b54efe51e37f160c769e4c8e812bafdd29fe
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-22 16:05:54 -0300

    feat: home page with cards and header

--- keenerfy-front/src/pages/Home/index.js
+++ keenerfy-front/src/pages/Home/index.js
@@ -1,7 +1,17 @@
+import Card from "../../components/Card"
+import Header from "../../components/Header"
+import './Home.css'
+
 const Home = () =>{
     return (
         <div>
-
+            <Header/>
+            <section id="list-cards">
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+            </section>
         </div>
     )
 }
