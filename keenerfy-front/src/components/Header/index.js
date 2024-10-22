328a80c16a4b3bf25736d039ae9618a4687d1b1c
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-22 16:04:55 -0300

    feat: all the header component implemented

--- keenerfy-front/src/components/Header/index.js
+++ keenerfy-front/src/components/Header/index.js
@@ -1,10 +1,31 @@
-import './Banner.css'
+import ButtonHeader from '../ButtonHeader'
+import './Header.css'
 
 function Header(){
     return (
-        <div className="banner">
-            <img src="/images/keener.fy.png" alt="icon from header of the page"/>
-        </div>
+            <div className='container-header'>
+                <header>
+                    <div id="img-container">
+                        <img src="/images/keener.fy.png" alt="icon from header of the page"/>
+                    </div>
+                    <div id="options-container">
+                        <ButtonHeader name="HOME"/>
+                        <ButtonHeader name="MY PURCHASES"/>
+                        <ButtonHeader name="NEW PRODUCT"/>
+                        <ButtonHeader name="SALES"/>
+                        <ButtonHeader name="STOCK"/>
+                    </div>
+                    <div id="user-container">
+                        <div>
+                        <i class="fas fa-user"></i>
+                        <p>YOUR USERNAME</p>
+                        </div>
+                        <i class="fas fa-sign-out"></i>
+
+                    </div>
+                        
+                </header>
+            </div>
     )
 }
 
