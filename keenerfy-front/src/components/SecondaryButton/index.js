07d6f42ec80309d68dbfc151071f9277f1d56d13
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-22 16:05:14 -0300

    feat: secondary button for login/register page

---
+++ keenerfy-front/src/components/SecondaryButton/index.js
@@ -0,0 +1,9 @@
+import './SecondaryButton.css'
+
+const SecondaryButton = (props) => {
+    return (
+            <button id="secondaryButton">{props.button}</button>
+    )
+}
+
+export default SecondaryButton
