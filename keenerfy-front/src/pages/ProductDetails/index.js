d6e44cb7d991ef44b559577a8d13ce389d89459e
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-22 19:49:46 -0300

    feat: implementing the page product-detail

--- keenerfy-front/src/pages/ProductDetails/index.js
+++ keenerfy-front/src/pages/ProductDetails/index.js
@@ -1,11 +1,30 @@
+import Button from "../../components/Button"
 import Header from "../../components/Header"
+import './ProductDetails.css'
 
 const ProductDetails = () =>{
     return (
-        <div>
-            <Header/>
+        <div className="container-new-product">
+            <div>
+                <Header/>
+            </div>
+            <form className="form-product-detail">
+                <h1>PRODUCT NAME</h1>
+                <hr class="custom-hr"/>
+                <div id="product-detail-block">
+                    <div id="img-detail-block">
+                        <img src="/images/kenner-shrek.png" alt="shrek's best flip flop"/>
+                    </div>
+                    <div>
+                        <h1>R$100.99</h1>
+                        <p>THIS IS AN AUTHENTIC PRODUCT CREATED BY KEENER.FY COMPANY</p>
+                        <Button button="BUY"/>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
+
 
 export default ProductDetails
