9e6a2c9df10dd133d49fbae68d6a3a2318d813d9
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-22 16:06:55 -0300

    feat: login and register page with the fields and button

--- keenerfy-front/src/pages/Login/index.js
+++ keenerfy-front/src/pages/Login/index.js
@@ -1,7 +1,19 @@
+import './Login.css'
+import TextField from '../../components/TextField'
+import Button from '../../components/Button'
+import SecondaryButton from '../../components/SecondaryButton'
+
 const Login = () =>{
     return (
-        <div>
-
+        <div className="container-login">
+                <img src="/images/keener.fy-big.svg" alt="Principal"/>
+                <form>
+                    <TextField label="LOGIN" placeholder="email@example.com"/>
+                    <TextField label="PASSWORD" placeholder="************"/>
+                    <h3>Forgot your password?</h3>
+                    <Button button="ENTER"/>
+                    <SecondaryButton button="REGISTER"/>
+                </form>
         </div>
     )
 }
--- keenerfy-front/src/pages/Register/index.js
+++ keenerfy-front/src/pages/Register/index.js
@@ -1,8 +1,34 @@
+import './Register.css'
+import TextField from '../../components/TextField'
+import Button from '../../components/Button'
+import SecondaryButton from '../../components/SecondaryButton'
+
 const Register = () =>{
     return (
-        <div>
+            <div className="container-register">
+                <img src="/images/keener.fy-big.svg" alt="Principal"/>
+                <form>
+                    <div className="sub-container">
+                    <TextField label="NAME" placeholder="Paulo Gustavo"/>
+                    <TextField label="LASTNAME" placeholder="Carvalho Tuler"/>
 
-        </div>
+                    </div>
+                    <div className="sub-container">
+                    <TextField label="CPF" placeholder="ex: 000.111.222-33"/>
+                    <TextField label="EMAIL" placeholder="example@mail.com"/>
+
+                    </div >
+                    <div className="sub-container">
+
+                    <TextField label="PASSWORD" placeholder="************"/>
+                    <TextField label="REPEAT YOUR PASSWORD" placeholder="************"/>
+                    </div>
+                    <div id="sub-container-button">
+                        <Button button="REGISTER"/>
+                        <SecondaryButton button="ALREADY CREATED?"/>
+                    </div>
+                </form>
+            </div>
     )
 }
 
