f280198246e478d767f5202c8d08b47622a9ab71
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-22 16:04:34 -0300

    feat: new component card that thas the product

---
+++ keenerfy-front/src/components/Card/index.js
@@ -0,0 +1,16 @@
+import './Card.css'
+
+const Card = () => {
+    return (
+            <div className='container-card'>
+                <article>
+                    <img src='/images/kenner-ben10.png' alt='ben10 kenner'></img>
+                    <h2>Card title</h2>
+                    <h2>R$109.55</h2>
+                    <h4>See more</h4>
+                </article>
+            </div>
+    )
+}
+
+export default Card
