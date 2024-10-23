f1bb1497420372a4d80e347c919ff66a92a2d21d
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-23 06:35:47 -0300

    feat: new component that is a list of table like excel

---
+++ keenerfy-front/src/components/ListTable/index.js
@@ -0,0 +1,34 @@
+import "./ListTable.css";
+import React from 'react';
+
+
+const ListTable = ({columns, data, title}) => {
+
+  return (
+    <div className="container-list-table">
+      <form className="form-list-table">
+        <h2>{title}</h2>
+        <hr class="custom-hr" />
+        <div className="sub-container-list-table">
+        </div>
+        <div className="grid-container">
+        {columns.map((col, index) => (
+          <div key={index} className="grid-header">
+            {col}
+          </div>
+        ))}
+      
+      {data.map((row, rowIndex) => (
+          columns.map((col, colIndex) => (
+            <div key={`${rowIndex}-${colIndex}`} className="grid-item">
+              {row[col]}
+            </div>
+          ))
+        ))}
+    </div>
+      </form>
+    </div>
+  );
+};
+
+export default ListTable;
