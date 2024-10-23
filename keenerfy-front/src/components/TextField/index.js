6f19f7bb549d1db62e037605f8a22a460790138c
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-23 06:36:44 -0300

    feat: new input mask to the cpf field

--- keenerfy-front/src/components/TextField/index.js
+++ keenerfy-front/src/components/TextField/index.js
@@ -1,11 +1,36 @@
 import './TextField.css'
+import InputMask from 'react-input-mask'
 
-const TextField = (props) => {
+const TextField = ({label, placeholder, value, onChange, type}) => {
     return (
         <div className="form">
             <div className="text-field">
-                <label>{props.label}</label>
-                <input placeholder={props.placeholder}/>
+                <label>{label}</label>
+                {label === 'CPF' ? 
+                    <InputMask
+                    mask="999.999.999-99" // Formato da máscara de CPF
+                    value={value}
+                    onChange={onChange}
+                    // Adicionando um evento para que o cursor não saia da posição
+                    onBlur={onChange} 
+                    >
+                    {(inputProps) => 
+                        <input 
+                            {...inputProps}  // Passa as propriedades do InputMask
+                            label={label} 
+                            placeholder={placeholder}
+                            value={value} // O valor atual do CPF
+                            type={type} // Definindo o tipo como "text"
+                        />
+                    }
+                    </InputMask>
+                : 
+                    <input 
+                    placeholder={placeholder}
+                    value={value}
+                    onChange={onChange}
+                    type={type ?? undefined}
+                    />}
             </div>
         </div>
     )
