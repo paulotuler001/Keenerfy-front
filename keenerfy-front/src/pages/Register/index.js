b4fa79c210e7394b97569db12fb49a856d494423
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-23 06:30:11 -0300

    feat: register now can conversate with back-end

--- keenerfy-front/src/pages/Register/index.js
+++ keenerfy-front/src/pages/Register/index.js
@@ -2,26 +2,96 @@ import './Register.css'
 import TextField from '../../components/TextField'
 import Button from '../../components/Button'
 import SecondaryButton from '../../components/SecondaryButton'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Register = () =>{
+
+    const [name, setName] = useState('')
+    const [lastname, setLastname] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [cpf, setCpf] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        // Dados que você quer enviar
+        const postData = {
+            username: name + " " + lastname,
+            email: email,
+            password: password,
+            cpf: cpf
+        };
+
+        // Fazendo uma requisição POST
+        axios.post('http://localhost:5006/register/register', postData)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error =>{
+                console.log(error);
+            })
+    };
+
+    const handleCPFChange = (e) => {
+        setCpf(e.target.value);
+    };
+
     return (
             <div className="container-register">
                 <img src="/images/keener.fy-big.svg" alt="Principal"/>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="sub-container">
-                    <TextField label="NAME" placeholder="Paulo Gustavo"/>
-                    <TextField label="LASTNAME" placeholder="Carvalho Tuler"/>
+                    <TextField 
+                    label="NAME" 
+                    placeholder="Paulo Gustavo"
+                    value={name} 
+                    type='name'
+                    onChange={e => setName(e.target.value)}
+                    />
+                    <TextField 
+                    label="LASTNAME" 
+                    placeholder="Carvalho Tuler" 
+                    value={lastname} 
+                    type='name'
+                    onChange={e => setLastname(e.target.value)} 
+                    />
 
                     </div>
                     <div className="sub-container">
-                    <TextField label="CPF" placeholder="ex: 000.111.222-33"/>
-                    <TextField label="EMAIL" placeholder="example@mail.com"/>
+                    <TextField 
+                        label="CPF" 
+                        placeholder="Digite seu CPF"
+                        value={cpf} // O valor atual do CPF
+                        onChange={handleCPFChange}
+                        type='text'
+                    />
+
+                    <TextField 
+                    label="EMAIL" 
+                    placeholder="example@mail.com"
+                    value={email} 
+                    type='email'
+                    onChange={e => setEmail(e.target.value)}
+                    />
 
                     </div >
                     <div className="sub-container">
 
-                    <TextField label="PASSWORD" placeholder="************"/>
-                    <TextField label="REPEAT YOUR PASSWORD" placeholder="************"/>
+                    <TextField 
+                    label="PASSWORD" 
+                    placeholder="************"
+                    value={password} 
+                    type='password'
+                    onChange={e => setPassword(e.target.value)}
+                    />
+                    <TextField 
+                    label="REPEAT YOUR PASSWORD" 
+                    placeholder="************"
+                    value={password} 
+                    type='password'
+                    onChange={e => setPassword(e.target.value)}
+                    />
                     </div>
                     <div id="sub-container-button">
                         <Button button="REGISTER"/>
