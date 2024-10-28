5ed49cafcaf822a0ed228f73c654695393458c15
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-28 20:22:27 -0300

    remove: comments unnecessarily

--- keenerfy-front/src/components/TextField/index.js
+++ keenerfy-front/src/components/TextField/index.js
@@ -9,19 +9,18 @@ const TextField = ({label, placeholder, value, onChange, type, required}) => {
                 <label>{label}</label>
                 {label === 'CPF' ? 
                     <InputMask
-                    mask="999.999.999-99" // Formato da máscara de CPF
+                    mask="999.999.999-99"
                     value={value}
                     onChange={onChange}
-                    // Adicionando um evento para que o cursor não saia da posição
                     onBlur={onChange} 
                     >
                     {(inputProps) => 
                         <input 
-                            {...inputProps}  // Passa as propriedades do InputMask
+                            {...inputProps} 
                             label={label} 
                             placeholder={placeholder}
-                            value={value} // O valor atual do CPF
-                            type={type} // Definindo o tipo como "text"
+                            value={value}
+                            type={type}
                         />
                     }
                     </InputMask>
