e7a0a206e673361386e956bd173ec1252acd3f54
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-28 20:21:45 -0300

    feat: icon when the button is to edit

--- keenerfy-front/src/components/Button/index.js
+++ keenerfy-front/src/components/Button/index.js
@@ -6,6 +6,8 @@ const Button = (props) => {
     return (
             props.button === "REGISTER" ? <Link to={"/login"}><button className='btn' type={props.type} onClick={props.onClick}>{props.button}</button></Link>
             : 
+            props.button === "EDIT" ? <button className='btn' type={props.type} onClick={props.onClick}><i className="fas fa-edit"></i></button>
+            :
             <button className='btn' type={props.type} onClick={props.onClick}>{props.button}</button>
     )
 }
