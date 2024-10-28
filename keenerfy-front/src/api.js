21411294ea1a68bd544c8f268c3791c7a61ea696
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-28 19:04:32 -0300

    feat: using Bearer and setting this in the headers

--- keenerfy-front/src/api.js
+++ keenerfy-front/src/api.js
@@ -3,3 +3,15 @@ import axios from "axios";
 export default axios.create({
     baseURL: "http://localhost:5006"
 })
+
+axios.interceptors.request.use(function (config) {
+    const token = localStorage.getItem('token')
+  
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config;
+  }, function (error) {
+  
+    return Promise.reject(error);
+  });
