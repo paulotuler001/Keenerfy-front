abec1f61344d5db54d5422f9f8e0532b4feb4baa
Paulo Gustavo Carvalho Tuler <paulogustavotuler@hotmail.com>
2024-10-28 18:58:36 -0300

    feat: navigate to login when you register

--- keenerfy-front/src/components/Button/index.js
+++ keenerfy-front/src/components/Button/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Button.css'
+import { Link } from 'react-router-dom';
 
 const Button = (props) => {
     return (
+            props.button === "REGISTER" ? <Link to={"/login"}><button className='btn' type={props.type} onClick={props.onClick}>{props.button}</button></Link>
+            : 
             <button className='btn' type={props.type} onClick={props.onClick}>{props.button}</button>
     )
 }
